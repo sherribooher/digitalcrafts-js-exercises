c675759e7549b9c2cd4d339748b13700b43205e2
Sherri Booher <sherribooher74@gmail.com>
2019-05-04 14:00:47 -0400

    Added solution for print-numbers

---
+++ javascript101/js-exercises/print-numbers.js
@@ -0,0 +1,9 @@
+// Write a function printNumbers which is given a start number and an end number. It will print the numbers from one to the other, one per line.
+
+function printNumbers(num1, num2) {
+  for (i = 1; i <= num2; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers(1, 10);
