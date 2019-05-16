2b9705347bd317fc9951eb1352d7ea356d77f0fb
Sherri Booher <sherribooher74@gmail.com>
2019-05-16 19:44:45 -0400

    Added solution for print-banner.js

--- javascript101/js-exercises/print-banner.js
+++ javascript101/js-exercises/print-banner.js
@@ -1,1 +1,21 @@
 // Write a function printBanner which is given some text and prints a banner with a border surrounding the text. The border has to stretch the length of the text.
+
+function printBanner(text) {
+  var length = text.length;
+  var rowString = "";
+  for (var row = 0; row < 3; row ++) {
+    switch (row) {
+      case 0:
+        rowString = "*".repeat(length + 4);
+        break;
+      case 1:
+        rowString = "* " + text + " *";
+        break;
+      case 2:
+        rowString = "*".repeat(length + 4);
+        break;
+    }
+    console.log(rowString);
+  }
+}
+printBanner("Welcome to DigitalCrafts!");
